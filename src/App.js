bb82c92574b69e0513300c958205c337273f709c
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-16 13:09:50 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -9,15 +8,16 @@ import BookDiscussion from './components/BookDiscussion'
 import BookIndex from './components/BookIndex'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-        <BookIndex />
-        <BookDiscussion />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Route exact path='/' component={BookIndex} />
+                    <Route exact path='/discussion' component={BookDiscussion} />
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
