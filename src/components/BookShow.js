db189431922348fe0c744d791bd8e9de55f90458
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-16 19:47:15 -0400

    saving

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom'
+import {NavLink} from 'react-router-dom';
+import { Panel } from 'react-bootstrap';
 export default class BookShow extends React.Component {
     
     render() {
         return(
-        <div>
-            <img src={this.props.book.url} alt="cover"/>
-            <h1>{this.props.book.title}</h1>
-            <h1>{this.props.book.author}</h1>
+        <Panel>
+            <img width={128} src={this.props.book.url} alt="cover" thumbnail />
+            <p className="author">{this.props.book.title}</p>
+            <p className="title">{this.props.book.author}</p>
                 <NavLink exact to={`/discussion/${this.props.book.id}`}>Discussion</NavLink>
-        </div>
+        </Panel>
         )
     }
 }
---
+++ src/containers/BookSearch.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export class BookSearch extends React.Component {
+    
+    render() {
+        return(
+            <p>Search</p>
+        ) 
+    }
+}
