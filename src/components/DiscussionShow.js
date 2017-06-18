8ea085389ed72c6e8c48bcb066d46c4bbf399631
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-18 14:31:59 -0400

    Discussion panel scrolls to bottom

--- src/components/DiscussionShow.js
+++ src/components/DiscussionShow.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import '../css/discussion.css';
-
+import { Scrollbars } from 'react-custom-scrollbars';
 export default class DiscussionShow extends React.Component {
 
     render() {
         var divStyle = {
             backgroundColor: 'white',
             height: '420px',
-            overflow: 'auto',
+
         }
         let comments = this.props.comments.map( (comment,index) =>{
             return (<p key={index}><span>{comment.username} (pg {comment.page}):</span> {comment.text}</p>)
         })
+
         return (
             <div>
                 <p>Discussion Panel</p>
-                <div style={divStyle} id='discussion-panel'>
+                <Scrollbars ref={ (scrollbars) => {this._scrollbars = scrollbars; }}
+                   style={divStyle}
+                    autoHide
+                    autoHideTimeout={1000}
+                    autoHideDuration={200}
+                    id='discussion-panel'>
                     {comments}
-                </div>
+                </Scrollbars>
             </div>
         )
+    }
+    componentDidMount() {
+        this._scrollbars.scrollToBottom();
+    }
+    componentDidUpdate() {
+        this._scrollbars.scrollToBottom();
     }
 }
