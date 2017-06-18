9d7d6b98201ebfd1a2e4e281b2a36712d6859abf
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-18 13:57:17 -0400

    custom scrollbars

--- src/components/DiscussionShow.js
+++ src/components/DiscussionShow.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import '../css/discussion.css';
 
 export default class DiscussionShow extends React.Component {
-    
+
     render() {
+        var divStyle = {
+            backgroundColor: 'white',
+            height: '420px',
+            overflow: 'auto',
+        }
         let comments = this.props.comments.map( (comment,index) =>{
             return (<p key={index}><span>{comment.username} (pg {comment.page}):</span> {comment.text}</p>)
         })
         return (
-            
             <div>
-               <p>Discussion Panel</p>
-                {comments}
+                <p>Discussion Panel</p>
+                <div style={divStyle} id='discussion-panel'>
+                    {comments}
+                </div>
             </div>
         )
     }
--- src/reducers/DiscussionsReducer.js
+++ src/reducers/DiscussionsReducer.js
@@ -1,8 +1,24 @@
 let initialState = [ 
-        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 1},
-        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 1},    
-        { username: "mdo5004", page: 111, text: "This is not a good book", bookId: 2},
-        { username: "rvo1987", page: 222, text: "Yes it is.", bookId: 2}
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is a good book", bookId: 181},
+        { username: "rvo1987", page: 222, text: "No it's not.", bookId: 181},    
+        { username: "mdo5004", page: 111, text: "This is not a good book", bookId: 221},
+        { username: "rvo1987", page: 222, text: "Yes it is.", bookId: 221}
 ]
 
 const discussionsReducer = (state=initialState, action) => {
