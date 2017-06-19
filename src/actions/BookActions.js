4fb6d2a64600dd82411d0df68033204433d576a5
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-06-19 10:10:53 -0400

    routes changed

--- src/actions/BookActions.js
+++ src/actions/BookActions.js
@@ -1,6 +1,15 @@
 export function addBook(index) {
     return { type: 'ADD_BOOK', payload: index}
 }
-export function removeBook(book){
+export function removeBook(book) {
     return { type: 'REMOVE_BOOK', payload: book}
+}
+export function loadBooks() {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_BOOKS'});
+        return fetch('/books', {
+            accept: 'application/json',
+        }).then(response => response.json())
+            .then( results => dispatch({type:"GET_BOOKS", payload: results}))
+    }
 }
