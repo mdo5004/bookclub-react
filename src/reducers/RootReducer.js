7a81388701d266728510177bcbc3ab0db64147ad
Michael OConnell <michaeldavidoconnell@gmail.com>
2017-06-27 12:55:26 -0400

    sessions

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -3,12 +3,13 @@ import booksReducer from './BooksReducer'
 import usersReducer from './UsersReducer'
 import discussionsReducer from './DiscussionsReducer'
 import searchResultsReducer from './SearchResultsReducer'
-
+import auth from './SessionsReducer'
 const rootReducer = combineReducers({
     books: booksReducer,
     discussions: discussionsReducer,
     user: usersReducer,
     searchResults: searchResultsReducer,
+    session: auth,
 });
 
 export default rootReducer;
